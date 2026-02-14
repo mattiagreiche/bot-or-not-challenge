a6888147d94438398ebdc1e98b5ea9b10522965a
mattiagreiche <mattiagreiche@gmail.com>
2026-02-13 23:10:31 -0500

    Update to integrate embeddings in data flow (and refactor)

--- build_user_features.py
+++ build_user_features.py
@@ -1,31 +1,44 @@
+import os
 from collections import Counter
 import re
 import string
 import json
 import math
 import pandas as pd
+from get_embeddings import get_embeddings
 
 # hi! this file extracts the features from the four datasets and puts them into a parquet file for use in the XGBoost model.
-# currently planning to create a function in a different file and using an open-source language model (tuned on twitter) to
-# get average features of a user's tweets/text, which should prove useful.
+# some features are easily extractable, and the others are embeddings from a sentence transformer model. you can change if
+# you want to include these embeddings or not below in USE_EMBEDDINGS
 
 # config
 
+USE_EMBEDDINGS = True
+
 POST_FILES = [
-    "training_data/dataset.posts&users.30.json",
-    "training_data/dataset.posts&users.31.json",
-    "training_data/dataset.posts&users.32.json",
-    "training_data/dataset.posts&users.33.json",
+    "raw_data/dataset.posts&users.30.json",
+    "raw_data/dataset.posts&users.31.json",
+    "raw_data/dataset.posts&users.32.json",
+    "raw_data/dataset.posts&users.33.json",
 ]
 
 BOT_FILES = [
-    "training_data/dataset.bots.30.txt",
-    "training_data/dataset.bots.31.txt",
-    "training_data/dataset.bots.32.txt",
-    "training_data/dataset.bots.33.txt",
+    "raw_data/dataset.bots.30.txt",
+    "raw_data/dataset.bots.31.txt",
+    "raw_data/dataset.bots.32.txt",
+    "raw_data/dataset.bots.33.txt",
 ]
 
-OUTPUT_PATH = "training_data/user_features.parquet"
+if USE_EMBEDDINGS:
+    OUTPUT_PATH = "training_data/user_features.parquet"
+    PREVIEW_PATH = "training_data/user_features_preview.csv"
+else:
+    OUTPUT_PATH = "training_data/user_features_no_emb.parquet"
+    PREVIEW_PATH = "training_data/user_features_no_emb_preview.csv"
+
+# makes dirs if non-existent
+os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
+os.makedirs(os.path.dirname(PREVIEW_PATH), exist_ok=True)
 
 
 # text helpers
@@ -38,6 +51,9 @@ def count_caps(s):
 
 def count_punct(s):
     return sum(1 for c in s if c in string.punctuation)
+
+def count_exclam(s):
+    return sum(1 for c in s if c == '!')
 
 def count_hashtags(s):
     return len(HASHTAG_RE.findall(s or "")) # avoids empty string errors
@@ -55,15 +71,6 @@ def char_entropy(s):
     return ent
 
 
-# language model integration
-
-def get_text_embeddings(text_series):
-    pass
-
-def get_description_embeddings(description_series):
-    pass
-
-
 # core feature extraction
 
 def extract_features_from_posts(posts_list):
@@ -75,55 +82,62 @@ def extract_features_from_posts(posts_list):
     posts["text"] = posts["text"].fillna("")
     
     # sort by user then time for time-diff calculations
-    posts = posts.sort_values(["author_id", "created_at"])
+    posts = posts.sort_values(["author_id", "created_at"]).reset_index(drop=True)
     
     # per-post features
     posts["len"] = posts["text"].str.len()
     posts["num_caps"] = posts["text"].map(count_caps)
     posts["num_punct"] = posts["text"].map(count_punct)
     posts["num_hashtags"] = posts["text"].map(count_hashtags)
+    posts["num_exclams"] = posts["text"].map(count_exclam)
     
     # time between posts
     posts["delta_s"] = posts.groupby("author_id")["created_at"].diff().dt.total_seconds()
     
-    # INSERT LM POST EMBEDDINGS HERE LATER
-    # post_embeddings = get_text_embeddings(posts["text"])
-    # posts = pd.concat([posts, post_embeddings], axis=1)
-    
     # aggregate per user and get a bunch of metrics
-    agg = posts.groupby("author_id").agg({
+    posts_agg = posts.groupby("author_id").agg({
         "delta_s": ["mean", "min", "std", "median"],
         "len": ["mean", "min", "max", "std"],
         "num_caps": ["mean", "min", "max", "std"],
         "num_punct": ["mean", "min", "max", "std"],
         "num_hashtags": ["mean", "min", "max", "std"],
+        "num_exclams": ["mean", "min", "max", "std"],
         "text": "count",
         "created_at": ["min", "max"],
     })
     
     # flatten columns
-    agg.columns = ["_".join(col).strip() for col in agg.columns]
-    agg = agg.reset_index()
-    agg = agg.rename(columns={"text_count": "post_count"})
+    posts_agg.columns = ["_".join(col).strip() for col in posts_agg.columns]
+    posts_agg = posts_agg.reset_index()
+    posts_agg = posts_agg.rename(columns={"text_count": "post_count"})
     
     # activity
-    agg["active_span_s"] = (agg["created_at_max"] - agg["created_at_min"]).dt.total_seconds()
+    posts_agg["active_span_s"] = (posts_agg["created_at_max"] - posts_agg["created_at_min"]).dt.total_seconds()
     
-    agg["posts_per_day"] = agg["post_count"] / (agg["active_span_s"] / 86400 + 1e-9) # avoid division by zero if one post
+    posts_agg["posts_per_day"] = posts_agg["post_count"] / (posts_agg["active_span_s"] / 86400 + 1e-9) # avoid division by zero if one post
     
     # fix delta_s to 0 for single posters
-    delta_cols = [c for c in agg.columns if c.startswith("delta_s_")]
+    delta_cols = [c for c in posts_agg.columns if c.startswith("delta_s_")]
     for col in delta_cols:
-        agg[col] = agg[col].fillna(0)
+        posts_agg[col] = posts_agg[col].fillna(0)
 
     # for single posters, posts_per_day = 1/2
-    agg["single_post"] = (agg["post_count"] == 1).astype(int)
-    agg.loc[agg["single_post"] == 1, "posts_per_day"] = 0.5 # time period is 2 days in the datasets    
+    posts_agg["single_post"] = (posts_agg["post_count"] == 1).astype(int)
+    posts_agg.loc[posts_agg["single_post"] == 1, "posts_per_day"] = 0.5 # time period is 2 days in the datasets    
     
-    agg = agg.drop(columns=["created_at_min", "created_at_max"])
-    agg = agg.fillna(0) # fill NaNs from std dev calculations on single posts
+    posts_agg = posts_agg.drop(columns=["created_at_min", "created_at_max"])
+    posts_agg = posts_agg.fillna(0) # fill NaNs from std dev calculations on single posts
     
-    return agg
+    if not USE_EMBEDDINGS:
+        return posts_agg
+    
+    # get LM embeddings
+    post_embeddings = get_embeddings(posts, "text")
+    
+    # merge
+    post_feats = pd.merge(posts_agg, post_embeddings, on="author_id", how="inner")
+    
+    return post_feats
 
 def extract_features_from_users(users_list):
     users = pd.DataFrame(users_list)
@@ -157,24 +171,37 @@ def extract_features_from_users(users_list):
     users["location_present"] = (loc != "").astype(int)
     users["location_len"] = loc.str.len()
     
-    # keep only useful features + author_id
+    if not USE_EMBEDDINGS:
+        # keep only useful features (no embeddings) + author_id
+        keep_cols = [
+            "author_id",
+            "username_len", "username_digits", "username_entropy",
+            "name_len", "name_nonalpha", "name_word_count", 
+            "description_len", "description_has_url", "description_has_hashtag", 
+            "description_has_mention", "missing_description",
+            "location_present", "location_len",
+        ]
+        return users[keep_cols]
+        
+    # get LM embeddings
+    user_embeddings = get_embeddings(users, "description")
+    
+    # merge
+    user_feats = pd.merge(users, user_embeddings, on="author_id", how="inner")
+    
+    # keep only useful/numerical features (INCL. embeddings) + author_id
     keep_cols = [
-        "author_id",
+        "author_id", "z_score",
         "username_len", "username_digits", "username_entropy",
         "name_len", "name_nonalpha", "name_word_count", 
         "description_len", "description_has_url", "description_has_hashtag", 
         "description_has_mention", "missing_description",
         "location_present", "location_len",
-    ]
+    ] + [c for c in user_feats.columns if c.startswith('emb')]
     
-    users = users[keep_cols]
+    user_feats = user_feats[keep_cols]
     
-        
-    # INSERT LM DESCRIPTION EMBEDDINGS HERE LATER
-    # desc_embeddings = get_description_embeddings(users["description"])
-    # users = pd.concat([users, desc_embeddings], axis=1)
-
-    return users
+    return user_feats
 
 def process_single_file(filepath):
     print(f"Processing {filepath}...")
@@ -204,7 +231,7 @@ def load_bot_labels(bot_files):
                 bot_ids.add(line.strip())
     return bot_ids
 
-# main
+
 
 def main():
     all_features = []
@@ -225,9 +252,8 @@ def main():
     final_df.to_parquet(OUTPUT_PATH, index=False)
     
     # prev
-    preview_path = "sample_preview.csv"
-    final_df.head(100).to_csv(preview_path, index=False)
-    print(f"Saved preview to {preview_path}")
+    final_df.head(10).to_csv(PREVIEW_PATH, index=False)
+    print(f"Saved preview to {PREVIEW_PATH}")
     print("Done.")
 
 if __name__ == "__main__":
